5b3a314a40390ed3b784426e23429a692c8d3090
jafar-j <jarrarja@grinnell.edu>
2026-03-31 23:29:40 -0500

    Fixed bug in adding new user

--- server/api/sql_init.py
+++ server/api/sql_init.py
@@ -68,7 +68,7 @@ def initialize_database():
     #print("Database initialized successfully.")
 
 def add_user(email, display_name=None, calendar_type=None, prefer_notify=0):
-    connection = sqlite3.connect('database.db')
+    connection = sqlite3.connect('test_grinvites.db')
     cursor = connection.cursor()
     cursor.execute('''
         INSERT OR IGNORE INTO users (email, display_name, calendar_type, prefer_notify)
@@ -78,7 +78,7 @@ def add_user(email, display_name=None, calendar_type=None, prefer_notify=0):
     connection.close()
 
 def get_user_by_email(email):
-    connection = sqlite3.connect('database.db')
+    connection = sqlite3.connect('test_grinvites.db')
     cursor = connection.cursor()
     cursor.execute('''
         SELECT * FROM users WHERE email = ?
@@ -87,7 +87,7 @@ def get_user_by_email(email):
     connection.close()
     return user
 
-init_db()
+initialize_database()
 add_user("juddbrau@gmail.com", "Judd Brau", "Google Calendar")
 user = get_user_by_email("juddbrau@gmail.com")
 print(user)
